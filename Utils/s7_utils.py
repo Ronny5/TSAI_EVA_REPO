d1161b9dd362cf9fd1fe65f2a143fc9235396303
Randeep Ghosh <randeep.ghosh05@gmail.com>
2020-09-13 02:32:12 +0530

    Update s7_utils.py

--- Utils/s7_utils.py
+++ Utils/s7_utils.py
@@ -11,13 +11,13 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-def cifar10_mean_std(dsname):
+def cifar10_mean_std():
     """Return the true mean of entire test and train dataset"""
     # simple transform
     simple_transforms = transforms.Compose([transforms.ToTensor(),])
     
-    exp_train = torchvision.datasets.dsname('./data', train=True, download=True, transform=simple_transforms)
-    exp_test = torchvision.datasets.dsname('./data', train=False, download=True, transform=simple_transforms)
+    exp_train = torchvision.datasets.CIFAR10('./data', train=True, download=True, transform=simple_transforms)
+    exp_test = torchvision.datasets.CIFAR10('./data', train=False, download=True, transform=simple_transforms)
     
     exp_tr_data = exp_train.data # train set
     exp_ts_data = exp_test.data # test set
