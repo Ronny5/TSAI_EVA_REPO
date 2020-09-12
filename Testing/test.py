8f4fb3feb471b078bd8c3a22ead83d7222b45b9e
Randeep Ghosh <randeep.ghosh05@gmail.com>
2020-09-13 02:59:10 +0530

    Update test.py

--- Testing/test.py
+++ Testing/test.py
@@ -10,6 +10,9 @@
 #      		Test Module		 	        #
 #########################################
 
+import torch
+import torchvision
+import torchvision.transforms as transforms
 
 def test(transform_var):
 	test_set  = torchvision.datasets.CIFAR10(root='./data', train=False,download=True, transform=transform_var)
